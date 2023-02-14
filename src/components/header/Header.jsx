e2833fc050e20279433cc32d1d2c61ab39e79831
JGaston89 <116741539+JGaston89@users.noreply.github.com>
2023-02-14 10:31:08 -0300

    Add files via upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './header.css'
-import cocacola from '../../assets/cocacola.png'
+import logohonda from '../../assets/logohonda.png'
 
 const Header = () => {
   return (
     <header>
         <div className="card">
             <div className="circle">
-                {/* <h1 className="logo">MOTORRADER</h1> */}
-                <img src={cocacola} className="logo" alt="" />
-            </div>            
+                <h1 className="logo">MOTORRADER</h1>
+            </div>  
+            <div className='content'>
+                <h2>Bienvenidos</h2>
+                <p>Lorem ipsum.</p>
+                <a href="#">Ir a</a>
+            </div>
+            <img src={logohonda} className="product_img" alt="" />         
         </div>
     </header>
   )
