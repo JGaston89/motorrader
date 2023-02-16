26db6a3246c7b919ad7195437e22d99fdc901a7b
JGaston89 <116741539+JGaston89@users.noreply.github.com>
2023-02-15 21:49:41 -0300

    Add files via upload

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <a href="#" className='footer__logo'></a>
+        <ul className='permalinks'>
+            <li><a href="Home"></a>Home</li>
+            <li><a href="Social"></a>Social</li>
+            <li><a href="Direccion"></a>Direccion</li>
+            <li><a href="Desde"></a>Desde</li>
+        </ul>
+
+
+    <div className='footer__copyright'>
+        <small>&copy; Gaston A. Jurado. All right reserverd.</small>
+    </div>
+
+    </footer>
   )
 }
 
